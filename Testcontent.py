dba564179718bdad0b172dcfe883182db00c16f8
AL-Young666 <71445509+alyang666@users.noreply.github.com>
2022-05-01 12:40:47 +0200

    Update Testcontent.py

--- Testcontent.py
+++ Testcontent.py
@@ -5,7 +5,7 @@ import string
 import sys
 
 def recommend(game_id,num):
-    gid=game_id+2000000
+    gid=game_id+2000000  # locate the gameid in file.csv
     idx=steam[steam["gameid"]== gid].index.tolist()[0]
     id_list = []
     for i in range(num):
@@ -17,7 +17,7 @@ def recommend(game_id,num):
     print(id_list)
 
 
-steam = pd.read_csv("D:\\Code\\DS50\\datasets\\test3.csv")
+steam = pd.read_csv("https://github.com/alyang666/DS50/blob/main/datastes/steam_id_score.csv")
 
 game_id = int(sys.argv[1])
 num = int(sys.argv[2])
