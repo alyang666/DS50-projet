90c9534e5a10ddd3e8576e08ef0b74ad73bc1ace
AL-Young666 <71445509+alyang666@users.noreply.github.com>
2022-06-09 17:50:06 +0200

    Add files via upload

--- collaborative filt.py
+++ collaborative filt.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import pairwise_distances
+import pymongo
 
+def load_from_mongo():
+    client = pymongo.MongoClient("mongodb+srv://jeremy:root@cluster0.5ei45.mongodb.net/test")
+    db = client.database
+    collection = db['steam_user']
+    df= pd.DataFrame(list(collection.find()))
+    del df['_id']
+    return df
 
 #Faire des prédictions sur la base de ces similitudes
 def predict(ratings, similarity, type='user'):
@@ -13,6 +21,7 @@ def predict(ratings, similarity, type='user'):
         pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
     return pred
 
+
 #Trouvez la différence entre les jeux joués pour l'objectif et l'utilisateur référé
 #Pour éviter de recommander pour objectif avec des jeux qui ont déjà été joués
 def find_diff(userid1,userid2):
@@ -20,7 +29,6 @@ def find_diff(userid1,userid2):
     game2=clean_data[clean_data["userid"]==userid2]["gameIdx"].tolist()
     diff = set(game2).difference(set(game1))
     return diff
-
 
 #Trier les jeux en fonction de leur scores et recommander les meilleurs.
 def sort_game(game_diff,useridx,similar_indices,num):
@@ -33,12 +41,18 @@ def sort_game(game_diff,useridx,similar_indices,num):
     sort=game_rating.argsort()[::-1]
     recomand_game=[0]*num
     for i in range(num):  
-        print(idx2game[list(game_diff)[sort[i]]])
-        print(clean_data.loc[clean_data['gameIdx']==list(game_diff)[sort[i]]]['appid'].tolist()[0])
+#        print(idx2game[list(game_diff)[sort[i]]])
+#        print(clean_data.loc[clean_data['gameIdx']==list(game_diff)[sort[i]]]['appid'].tolist()[0])
         recomand_game[i]=clean_data.loc[clean_data['gameIdx']==list(game_diff)[sort[i]]]['appid'].tolist()[0]
     return recomand_game
 
-
+#suppression des doublons
+def del_dup(items):
+    s = set()
+    for item in items:
+        if item not in s:
+            yield item
+            s.add(item)
 
 #Convertir l'identifiant de l'utilisateur en index
 #Trouver l'utilisateurs le plus similaire
@@ -68,9 +82,13 @@ def recommendation(userid,prediction,num):
                     recommended.append(recomand_game[i])
             num=num_rec
             i=i+1
-#Si le nombre de jeux qui peuvent être recommandés par des utilisateurs similaires est 
-#inférieur au nombre de jeux qui doivent être recommandés, les jeux les plus vendus seront recommandés.
-#Ne recommandez pas les jeux qui ont déjà été recommandés.
+    
+    #suppression des doublons
+    recommended=list(del_dup(recommended))
+    
+    #Si le nombre de jeux qui peuvent être recommandés par des utilisateurs similaires est 
+    #inférieur au nombre de jeux qui doivent être recommandés, les jeux les plus vendus seront recommandés.
+    #Ne recommandez pas les jeux qui ont déjà été recommandés.   
     if num>0:            
         if len(recommended)!=0:
             for j in range(len(recommended)):
@@ -78,16 +96,17 @@ def recommendation(userid,prediction,num):
                     if recommended[j] == topgames[i][0]:
                         del topgames[i]
         for k in range(num):
-            print(topgames[k])
+            recommended.append(topgames[k])
+    #Affiche l'identifiant du jeu recommandé
+    for x in recommended:
+        print(x,end=' ')
 
 
 
-
+clean_data = load_from_mongo()
 # clean_data = pd.read_csv('D:\Code\DS50\datasets\\user_data.csv',index_col=0)
-
 n_users = len(clean_data.userid.unique()) #Nombre d'utilisateurs après le traitement
 n_games = len(clean_data.game.unique())   #Nombre de jeux
-
 
 # Créer les dictionnaires pour convertir l'utilisateur et les jeux en index et inversement
 user2idx = {user: i for i, user in enumerate(clean_data.userid.unique())}
@@ -101,13 +120,12 @@ user_idx = clean_data['userid'].apply(lambda x: user2idx[x]).values
 game_idx = clean_data['gameIdx'] = clean_data['game'].apply(lambda x: game2idx[x]).values
 rating = clean_data['rating'].values
 
-
+# Créer une matrice zéro
 zero_matrix = np.zeros(shape = (n_users, n_games))
 user_game_interactions = zero_matrix.copy()
 # Remplir la matrice avec des scores
 user_game_interactions[user_idx, game_idx] = rating
 
-#Calcul de la similarité cosinus en utilisant pairwise de sklearn
 user_similarity = pairwise_distances(user_game_interactions, metric='cosine')
 item_similarity = pairwise_distances(user_game_interactions.T, metric='cosine')
 
@@ -125,7 +143,14 @@ topgames[7]=[8930,"Sid Meier's Civilization V"]
 topgames[8]=[301520,"Robocraft"]
 topgames[9]=[320,"Half-Life 2 Deathmatch"]
 
-recommendation(6717871,user_prediction,70)
 
+# input
+# userid = int(sys.argv[1])
+
+recommendation(6717871,user_prediction,20)
+
+# output
+# output = recommendation(userid, user_prediction, 20)
+# sys.stdout.write(output)
 
 
